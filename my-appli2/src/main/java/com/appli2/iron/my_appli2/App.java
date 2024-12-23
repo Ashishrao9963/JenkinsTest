a099e1a627c693f3cf82958c6f322e29ab2d7a9a
ShivanathDursheti <163499200+ShivanathDursheti@users.noreply.github.com>
2024-12-23 18:35:06 +0530

    Add files via upload

---
+++ my-appli2/src/main/java/com/appli2/iron/my_appli2/App.java
@@ -0,0 +1,13 @@
+package com.appli2.iron.my_appli2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
